7202bb1e62e117533585d4ec399aa275263febf9
blinkstar73 <chg2080@gmail.com>
2016-02-24 02:38:00 +0900

    GameOver clip update

---
+++ Assets/02.Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+    private Animator anim;
+    private bool isEndClip = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(GameManager.instance.isGameOver)
+        {
+            anim.SetTrigger("GameOver");
+        }
+
+        if(isEndClip && (Input.touchCount > 0 || Input.GetMouseButtonDown(0)))
+        {
+            SceneManager.LoadScene("scTitle");
+        }
+    }
+
+    public void EndGameOverClip()
+    {
+        isEndClip = true;
+    }
+}
